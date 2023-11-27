55679b42f6cda05dd15cdb23f1c41440f5f09fe3
André L. C. Moreira <andrelcmoreira@disroot.org>
2023-11-26 23:49:19 -0300

    ls: adding support to multiple entries

--- src/ls.rs
+++ src/ls.rs
@@ -1,9 +1,10 @@
 use std::env::args;
 use std::fs::read_dir;
 use std::io::ErrorKind::{NotFound, PermissionDenied};
+use std::path::Path;
 use std::process::exit;
 
-fn ls(path: &str) {
+fn ls_directory(path: &str) {
     let files = match read_dir(path) {
         Ok(f) => f,
         Err(e) => {
@@ -34,8 +35,28 @@ fn ls(path: &str) {
     println!("")
 }
 
+fn ls_file(path: &str) {
+    println!("{}", path)
+}
+
+fn is_directory(path: &str) -> bool {
+    let p = Path::new(path);
+
+    return p.is_dir()
+}
+
+fn ls(path: &str) {
+    if is_directory(path) {
+        ls_directory(path)
+    } else {
+        ls_file(path)
+    }
+}
+
 fn main() {
     let args: Vec<String> = args().collect();
 
-    ls(args[1].as_str())
+    for i in 1..args.len() {
+        ls(args[i].as_str())
+    }
 }
