3cf55791f28c645daa3cc1db84e4544c9ddea3ff
André L. C. Moreira <andrelcmoreira@disroot.org>
2023-12-02 13:37:12 -0300

    cat: handling errors on read_to_string call

--- src/cat.rs
+++ src/cat.rs
@@ -1,22 +1,34 @@
 use std::env::args;
 use std::fs::File;
 use std::io::Read;
+use std::panic::set_hook;
+
+fn set_panic_handler() {
+    set_hook(Box::new(|info| {
+        if let Some(s) = info.payload().downcast_ref::<String>() {
+            println!("{}", s);
+        }
+    }));
+}
 
 fn cat(filename: &str) {
     let mut buf = String::new();
 
-    match File::open(filename) {
-        Ok(mut f) => {
-            f.read_to_string(&mut buf).unwrap();
-            print!("{buf}")
-        },
-        Err(e) => println!("cat: {filename}: {}", e.to_string())
+    let mut file = match File::open(filename) {
+        Ok(f) => f,
+        Err(e) => panic!("cat: {filename}: {}", e.to_string())
+    };
+
+    match file.read_to_string(&mut buf) {
+        Ok(_) => print!("{buf}"),
+        Err(e) => panic!("cat: {filename}: {}", e.to_string())
     };
 }
 
 fn main() {
-    let args: Vec<String> = args().collect();
+    set_panic_handler();
 
+    let args: Vec<String> = args().collect();
     for arg in &args[1..] {
         cat(arg.as_str());
     }
