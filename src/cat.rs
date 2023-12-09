7dfe1714f4c9cf7f37544e86ebf2b7204d2652de
André L. C. Moreira <andrelcmoreira@disroot.org>
2023-12-08 21:53:28 -0300

    cat: adding the reading from stdin when no file is supplied

--- src/cat.rs
+++ src/cat.rs
@@ -1,30 +1,29 @@
 use std::env::args;
 use std::fs::File;
-use std::io::Read;
+use std::io::{stdin, Read};
 
 mod core;
 use core::error::set_panic_handler;
 
-enum CatFlags {
-    NoOption,
+enum Flags {
 }
 
-struct CatCtx {
+struct ProgCtx {
     files: Vec<String>,
-    flags: Vec<CatFlags>
+    flags: Vec<Flags>
 }
 
-impl CatCtx {
-    fn new() -> CatCtx {
-        CatCtx {
+impl ProgCtx {
+    fn new() -> ProgCtx {
+        ProgCtx {
             files: Vec::new(),
             flags: Vec::new()
         }
     }
 }
 
-fn build_ctx_from_args(args: Vec<String>) -> CatCtx {
-    let mut ctx = CatCtx::new();
+fn build_ctx(args: Vec<String>) -> ProgCtx {
+    let mut ctx = ProgCtx::new();
 
     for arg in &args[1..] {
         match arg.as_str() {
@@ -35,7 +34,28 @@ fn build_ctx_from_args(args: Vec<String>) -> CatCtx {
     return ctx
 }
 
-fn cat(filename: &str) {
+fn cat(ctx: ProgCtx) {
+    if ! ctx.files.is_empty() {
+        for file in ctx.files {
+            cat_from_file(file.as_str(), &ctx.flags);
+        }
+    } else {
+        cat_from_stdin()
+    }
+}
+
+fn cat_from_stdin() {
+    let mut buf = String::new();
+
+    loop {
+        stdin().read_line(&mut buf).unwrap();
+
+        print!("{buf}");
+        buf.clear()
+    }
+}
+
+fn cat_from_file(filename: &str, _flags: &Vec<Flags>) {
     let mut buf = String::new();
 
     let mut file = match File::open(filename) {
@@ -52,8 +72,6 @@ fn cat(filename: &str) {
 fn main() {
     set_panic_handler();
 
-    let ctx = build_ctx_from_args(args().collect());
-    for file in &ctx.files {
-        cat(file.as_str());
-    }
+    let ctx = build_ctx(args().collect());
+    cat(ctx)
 }
