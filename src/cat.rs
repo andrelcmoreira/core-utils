2af16b28ca26aa150ec2ba3846ef2db5f053b52c
André L. C. Moreira <andrelcmoreira@disroot.org>
2023-12-09 23:13:45 -0300

    cat: handling not existent parameters

--- src/cat.rs
+++ src/cat.rs
@@ -22,38 +22,54 @@ impl ProgCtx {
             flags: Vec::new()
         }
     }
+
+    fn has_option(&self, opt: Options) -> bool {
+        self.flags.contains(&opt)
+    }
+
+    fn has_files(&self) -> bool {
+        self.files.len() > 0
+    }
 }
 
 fn show_usage() {
-    let usage = format!(
+    let usage =
         "Usage: cat [OPTION]... [FILE]...\n\
          Concatenate FILE(S) to the standard output.\n\n\
          If FILE is not specified or be - , read the standard input.\n\n\
-         \t--help        display this help and exit");
+         \t--help        display this help and exit";
 
     println!("{usage}")
 }
 
-fn build_ctx(args: Vec<String>) -> ProgCtx {
+fn build_ctx(args: Vec<String>) -> Result<ProgCtx, String> {
     let mut ctx = ProgCtx::new();
 
     for arg in &args[1..] {
         match arg.as_str() {
             "--help" => ctx.flags.push(Options::Help),
-            _ => ctx.files.push(arg.clone())
+            _ => {
+                if arg.starts_with("-") {
+                    let msg = format!("cat: invalid option -- \"{arg}\"\n\
+                                      Try cat \"--help\" for more informations.");
+                    return Err(msg)
+                } else {
+                    ctx.files.push(arg.clone())
+                }
+            }
         }
     }
 
-    return ctx
+    Ok(ctx)
 }
 
 fn cat(ctx: ProgCtx) {
-    if ctx.flags.contains(&Options::Help) {
+    if ctx.has_option(Options::Help) {
         show_usage();
         return
     }
 
-    if ! ctx.files.is_empty() {
+    if ctx.has_files() {
         for file in ctx.files {
             cat_from_file(file.as_str(), &ctx.flags);
         }
@@ -90,6 +106,9 @@ fn cat_from_file(filename: &str, _flags: &Vec<Options>) {
 fn main() {
     set_panic_handler();
 
-    let ctx = build_ctx(args().collect());
-    cat(ctx)
+    let args = args().collect::<Vec<String>>();
+    match build_ctx(args) {
+        Ok(ctx) => cat(ctx),
+        Err(e) => panic!("{e}")
+    }
 }
