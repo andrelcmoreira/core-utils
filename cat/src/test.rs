159d3bb97f23f06199ecfe4f7783993a292246e0
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-01-20 16:21:39 -0300

    cat: adding implementation of test cases for parse_cli_args function

--- cat/src/test.rs
+++ cat/src/test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use crate::parse_cli_args;
+    use crate::*;
 
     #[test]
     fn parse_cli_args_with_no_args() {
@@ -8,6 +8,44 @@ mod tests {
         let ret = parse_cli_args(args).unwrap();
 
         assert_eq!(ret.inputs.is_empty(), true);
+        assert_eq!(ret.flags.is_empty(), true)
+    }
+
+    #[test]
+    fn parse_cli_args_with_hyphen_opt() {
+        let args = vec!["cat".to_string(), "-".to_string()];
+        let ret = parse_cli_args(args.clone()).unwrap();
+
+        assert_eq!(ret.inputs.contains(&InputParam::Stdin), true);
+        assert_eq!(ret.inputs.len(), args.len() - 1);
+        assert_eq!(ret.flags.is_empty(), true)
+    }
+
+    #[test]
+    fn parse_cli_args_with_single_file() {
+        let args = vec!["cat".to_string(), "foo".to_string()];
+        let ret = parse_cli_args(args.clone()).unwrap();
+
+        assert_eq!(ret.inputs.contains(&InputParam::File("foo".to_string())),
+                   true);
+        assert_eq!(ret.inputs.len(), args.len() - 1);
+        assert_eq!(ret.flags.is_empty(), true)
+    }
+
+    #[test]
+    fn parse_cli_args_with_multiple_files() {
+        let args = vec!["cat".to_string(),
+                        "foo".to_string(),
+                        "bar".to_string(),
+                        "baz".to_string()];
+        let ret = parse_cli_args(args.clone()).unwrap();
+
+        for file in &args[1..] {
+            assert_eq!(ret.inputs.contains(&InputParam::File(file.to_string())),
+                       true);
+        }
+
+        assert_eq!(ret.inputs.len(), args.len() - 1);
         assert_eq!(ret.flags.is_empty(), true)
     }
 }
