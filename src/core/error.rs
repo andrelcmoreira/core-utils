8ee0f48dae25b09fba71225f4749d155d66ea089
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-01-20 12:48:53 -0300

    cat: moving to a separate workspace

--- src/core/error.rs
+++
@@ -1,9 +0,0 @@
-use std::panic::set_hook;
-
-pub fn set_panic_handler() {
-    set_hook(Box::new(|info| {
-        if let Some(s) = info.payload().downcast_ref::<String>() {
-            println!("{}", s);
-        }
-    }));
-}
