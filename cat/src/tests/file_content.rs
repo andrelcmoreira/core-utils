b2ee65c855eb439eda927179a177b05d036461e1
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-01-24 22:08:29 -0300

    cat: adding tests for 'add_end_char' of FileContent trait

--- cat/src/tests/file_content.rs
+++ cat/src/tests/file_content.rs
@@ -39,3 +39,43 @@ fn add_line_number_to_empty_string() {
 
     assert_eq!(text, expected)
 }
+
+#[test]
+fn show_ends_to_multi_line_str() {
+    let mut text = "hello\nworld\nit's me\nagain\n".to_string();
+    let expected = "hello$\nworld$\nit's me$\nagain$\n".to_string();
+
+    text.add_end_char();
+
+    assert_eq!(text, expected)
+}
+
+#[test]
+fn show_ends_to_single_line_str() {
+    let mut text = "hello\n".to_string();
+    let expected = "hello$\n".to_string();
+
+    text.add_end_char();
+
+    assert_eq!(text, expected)
+}
+
+#[test]
+fn show_ends_to_no_lf_str() {
+    let mut text = "hello".to_string();
+    let expected = "hello".to_string();
+
+    text.add_end_char();
+
+    assert_eq!(text, expected)
+}
+
+#[test]
+fn show_ends_to_empty_string() {
+    let mut text = "".to_string();
+    let expected = "".to_string();
+
+    text.add_end_char();
+
+    assert_eq!(text, expected)
+}
