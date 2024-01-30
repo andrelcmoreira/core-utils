845bfc8116be4b8192f108cc968a78783d06651c
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-01-29 21:18:26 -0300

    cat: putting the unit tests inside the 'tests' module

--- cat/src/tests/file_content.rs
+++ cat/src/tests/file_content.rs
@@ -1,191 +1,194 @@
-use crate::*;
+#[cfg(test)]
+mod tests {
+    use crate::*;
 
-#[test]
-fn add_line_number_to_multi_line_str() {
-    let mut text = "hello\nworld\nit's me\nagain\n".to_string();
-    let expected = "1\thello\n2\tworld\n3\tit's me\n4\tagain\n".to_string();
+    #[test]
+    fn add_line_number_to_multi_line_str() {
+        let mut text = "hello\nworld\nit's me\nagain\n".to_string();
+        let expected = "1\thello\n2\tworld\n3\tit's me\n4\tagain\n".to_string();
 
-    text.add_line_number();
+        text.add_line_number();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_line_number_to_single_line_str() {
-    let mut text = "hello\n".to_string();
-    let expected = "1\thello\n".to_string();
+    #[test]
+    fn add_line_number_to_single_line_str() {
+        let mut text = "hello\n".to_string();
+        let expected = "1\thello\n".to_string();
 
-    text.add_line_number();
+        text.add_line_number();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_line_number_to_no_lf_str() {
-    let mut text = "hello".to_string();
-    let expected = "1\thello".to_string();
+    #[test]
+    fn add_line_number_to_no_lf_str() {
+        let mut text = "hello".to_string();
+        let expected = "1\thello".to_string();
 
-    text.add_line_number();
+        text.add_line_number();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_line_number_to_empty_string() {
-    let mut text = "".to_string();
-    let expected = "".to_string();
+    #[test]
+    fn add_line_number_to_empty_string() {
+        let mut text = "".to_string();
+        let expected = "".to_string();
 
-    text.add_line_number();
+        text.add_line_number();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_end_to_multi_line_str() {
-    let mut text = "hello\nworld\nit's me\nagain\n".to_string();
-    let expected = "hello$\nworld$\nit's me$\nagain$\n".to_string();
+    #[test]
+    fn add_end_to_multi_line_str() {
+        let mut text = "hello\nworld\nit's me\nagain\n".to_string();
+        let expected = "hello$\nworld$\nit's me$\nagain$\n".to_string();
 
-    text.add_end_char();
+        text.add_end_char();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_end_to_single_line_str() {
-    let mut text = "hello\n".to_string();
-    let expected = "hello$\n".to_string();
+    #[test]
+    fn add_end_to_single_line_str() {
+        let mut text = "hello\n".to_string();
+        let expected = "hello$\n".to_string();
 
-    text.add_end_char();
+        text.add_end_char();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_end_to_no_lf_str() {
-    let mut text = "hello".to_string();
-    let expected = "hello".to_string();
+    #[test]
+    fn add_end_to_no_lf_str() {
+        let mut text = "hello".to_string();
+        let expected = "hello".to_string();
 
-    text.add_end_char();
+        text.add_end_char();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_end_to_empty_string() {
-    let mut text = "".to_string();
-    let expected = "".to_string();
+    #[test]
+    fn add_end_to_empty_string() {
+        let mut text = "".to_string();
+        let expected = "".to_string();
 
-    text.add_end_char();
+        text.add_end_char();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_cr_to_multi_line_str() {
-    let mut text = "hello\r\nworld\r\nit's me\r\nagain\r\n".to_string();
-    let expected = "hello^M\nworld^M\nit's me^M\nagain^M\n".to_string();
+    #[test]
+    fn add_cr_to_multi_line_str() {
+        let mut text = "hello\r\nworld\r\nit's me\r\nagain\r\n".to_string();
+        let expected = "hello^M\nworld^M\nit's me^M\nagain^M\n".to_string();
 
-    text.add_cr();
+        text.add_cr();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_cr_to_str_without_cr() {
-    let mut text = "hello\nworld\nit's me\nagain\n".to_string();
-    let expected = "hello\nworld\nit's me\nagain\n".to_string();
+    #[test]
+    fn add_cr_to_str_without_cr() {
+        let mut text = "hello\nworld\nit's me\nagain\n".to_string();
+        let expected = "hello\nworld\nit's me\nagain\n".to_string();
 
-    text.add_cr();
+        text.add_cr();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_cr_to_str_with_single_line() {
-    let mut text = "hello\r\n".to_string();
-    let expected = "hello^M\n".to_string();
+    #[test]
+    fn add_cr_to_str_with_single_line() {
+        let mut text = "hello\r\n".to_string();
+        let expected = "hello^M\n".to_string();
 
-    text.add_cr();
+        text.add_cr();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_cr_to_str_without_crlf() {
-    let mut text = "hello".to_string();
-    let expected = "hello".to_string();
+    #[test]
+    fn add_cr_to_str_without_crlf() {
+        let mut text = "hello".to_string();
+        let expected = "hello".to_string();
 
-    text.add_cr();
+        text.add_cr();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_cr_to_empty_string() {
-    let mut text = "".to_string();
-    let expected = "".to_string();
+    #[test]
+    fn add_cr_to_empty_string() {
+        let mut text = "".to_string();
+        let expected = "".to_string();
 
-    text.add_cr();
+        text.add_cr();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_tab_char_to_str() {
-    let mut text = "hello\tworld\tit's me\tagain\n".to_string();
-    let expected = "hello^Iworld^Iit's me^Iagain\n".to_string();
+    #[test]
+    fn add_tab_char_to_str() {
+        let mut text = "hello\tworld\tit's me\tagain\n".to_string();
+        let expected = "hello^Iworld^Iit's me^Iagain\n".to_string();
 
-    text.add_tabs();
+        text.add_tabs();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_tab_to_str_without_tab() {
-    let mut text = "hello".to_string();
-    let expected = "hello".to_string();
+    #[test]
+    fn add_tab_to_str_without_tab() {
+        let mut text = "hello".to_string();
+        let expected = "hello".to_string();
 
-    text.add_tabs();
+        text.add_tabs();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn add_tab_to_empty_string() {
-    let mut text = "".to_string();
-    let expected = "".to_string();
+    #[test]
+    fn add_tab_to_empty_string() {
+        let mut text = "".to_string();
+        let expected = "".to_string();
 
-    text.add_tabs();
+        text.add_tabs();
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn replace_byte_with_empty_string() {
-    let mut text = "".to_string();
-    let expected = "".to_string();
+    #[test]
+    fn replace_byte_with_empty_string() {
+        let mut text = "".to_string();
+        let expected = "".to_string();
 
-    text.replace_byte(0xd, "@");
+        text.replace_byte(0xd, "@");
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn replace_byte_with_multi_line_string() {
-    let mut text = "hello\nworld!".to_string();
-    let expected = "he@@o\nwor@d!".to_string();
+    #[test]
+    fn replace_byte_with_multi_line_string() {
+        let mut text = "hello\nworld!".to_string();
+        let expected = "he@@o\nwor@d!".to_string();
 
-    text.replace_byte(0x6c, "@");
+        text.replace_byte(0x6c, "@");
 
-    assert_eq!(text, expected)
-}
+        assert_eq!(text, expected)
+    }
 
-#[test]
-fn replace_byte_with_single_line_string() {
-    let mut text = "hello, world!".to_string();
-    let expected = "he@@o, wor@d!".to_string();
+    #[test]
+    fn replace_byte_with_single_line_string() {
+        let mut text = "hello, world!".to_string();
+        let expected = "he@@o, wor@d!".to_string();
 
-    text.replace_byte(0x6c, "@");
+        text.replace_byte(0x6c, "@");
 
-    assert_eq!(text, expected)
+        assert_eq!(text, expected)
+    }
 }
