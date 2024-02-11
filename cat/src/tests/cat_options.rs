9d4d549611b99056a664288d0fe46351e63d030a
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-02-11 17:08:45 -0300

    cat: adding missing unit tests for cat_options suite

--- cat/src/tests/cat_options.rs
+++ cat/src/tests/cat_options.rs
@@ -3,7 +3,7 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn add_single_input_with_success() {
+    fn add_single_input() {
         let mut opts = CatOptions::new();
 
         opts.add_input(InputParam::Stdin);
@@ -13,7 +13,7 @@ mod tests {
     }
 
     #[test]
-    fn add_single_flag_with_success() {
+    fn add_single_flag() {
         let mut opts = CatOptions::new();
 
         opts.add_flag(FlagParam::ShowTabs);
@@ -23,23 +23,23 @@ mod tests {
     }
 
     #[test]
-    fn add_multiple_input_with_success() {
+    fn add_multiple_inputs() {
         let mut opts = CatOptions::new();
 
-        opts.add_input(InputParam::File("foo".to_string()));
-        opts.add_input(InputParam::File("bar".to_string()));
-        opts.add_input(InputParam::File("baz".to_string()));
-        opts.add_input(InputParam::File("qux".to_string()));
+        opts.add_input(InputParam::File("f1".to_string()));
+        opts.add_input(InputParam::File("f2".to_string()));
+        opts.add_input(InputParam::File("f3".to_string()));
+        opts.add_input(InputParam::File("f4".to_string()));
 
-        assert_eq!(opts.inputs.contains(&InputParam::File("foo".to_string())), true);
-        assert_eq!(opts.inputs.contains(&InputParam::File("bar".to_string())), true);
-        assert_eq!(opts.inputs.contains(&InputParam::File("baz".to_string())), true);
-        assert_eq!(opts.inputs.contains(&InputParam::File("qux".to_string())), true);
+        assert_eq!(opts.inputs.contains(&InputParam::File("f1".to_string())), true);
+        assert_eq!(opts.inputs.contains(&InputParam::File("f2".to_string())), true);
+        assert_eq!(opts.inputs.contains(&InputParam::File("f3".to_string())), true);
+        assert_eq!(opts.inputs.contains(&InputParam::File("f4".to_string())), true);
         assert_eq!(opts.inputs.len(), 4);
     }
 
     #[test]
-    fn add_multiple_flag_with_success() {
+    fn add_multiple_flags() {
         let mut opts = CatOptions::new();
 
         opts.add_flag(FlagParam::ShowTabs);
@@ -52,5 +52,50 @@ mod tests {
         assert_eq!(opts.flags.contains(&FlagParam::ShowNonPrinting), true);
         assert_eq!(opts.flags.contains(&FlagParam::ShowLineNumber), true);
         assert_eq!(opts.flags.len(), 4);
+    }
+
+    #[test]
+    fn has_flag_with_no_flags() {
+        let opts = CatOptions::new();
+
+        assert_eq!(opts.has_flag(FlagParam::ShowTabs), false);
+        assert_eq!(opts.has_flag(FlagParam::ShowEnds), false);
+        assert_eq!(opts.has_flag(FlagParam::ShowNonPrinting), false);
+        assert_eq!(opts.has_flag(FlagParam::ShowLineNumber), false);
+        assert_eq!(opts.has_flag(FlagParam::SqueezeBlank), false);
+        assert_eq!(opts.has_flag(FlagParam::NumberNonBlank), false);
+        assert_eq!(opts.has_flag(FlagParam::Help), false);
+    }
+
+    #[test]
+    fn has_flag_with_single_flag() {
+        let mut opts = CatOptions::new();
+
+        opts.add_flag(FlagParam::SqueezeBlank);
+
+        assert_eq!(opts.has_flag(FlagParam::ShowTabs), false);
+        assert_eq!(opts.has_flag(FlagParam::ShowEnds), false);
+        assert_eq!(opts.has_flag(FlagParam::ShowNonPrinting), false);
+        assert_eq!(opts.has_flag(FlagParam::ShowLineNumber), false);
+        assert_eq!(opts.has_flag(FlagParam::SqueezeBlank), true);
+        assert_eq!(opts.has_flag(FlagParam::NumberNonBlank), false);
+        assert_eq!(opts.has_flag(FlagParam::Help), false);
+    }
+
+    #[test]
+    fn has_flag_with_multiple_flags() {
+        let mut opts = CatOptions::new();
+
+        opts.add_flag(FlagParam::SqueezeBlank);
+        opts.add_flag(FlagParam::ShowNonPrinting);
+        opts.add_flag(FlagParam::ShowLineNumber);
+
+        assert_eq!(opts.has_flag(FlagParam::ShowTabs), false);
+        assert_eq!(opts.has_flag(FlagParam::ShowEnds), false);
+        assert_eq!(opts.has_flag(FlagParam::ShowNonPrinting), true);
+        assert_eq!(opts.has_flag(FlagParam::ShowLineNumber), true);
+        assert_eq!(opts.has_flag(FlagParam::SqueezeBlank), true);
+        assert_eq!(opts.has_flag(FlagParam::NumberNonBlank), false);
+        assert_eq!(opts.has_flag(FlagParam::Help), false);
     }
 }
