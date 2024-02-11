7d31f339fdcf4b6edcf474402e602698a2847de4
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-02-11 01:02:05 -0300

    cat: adding base test suite for cat struct

---
+++ cat/src/tests/cat.rs
@@ -0,0 +1,36 @@
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn cat_with_no_opts() {
+    }
+
+    #[test]
+    fn cat_with_squeeze_blank() {
+    }
+
+    #[test]
+    fn cat_with_show_ends() {
+    }
+
+    #[test]
+    fn cat_with_add_tabs() {
+    }
+
+    #[test]
+    fn cat_with_add_cr() {
+    }
+
+    #[test]
+    fn cat_with_number_non_blank() {
+    }
+
+    #[test]
+    fn cat_with_show_line_number() {
+    }
+
+    #[test]
+    fn cat_with_show_line_number_and_number_non_blank() {
+    }
+}
--- cat/src/tests/mod.rs
+++ cat/src/tests/mod.rs
@@ -1,3 +1,4 @@
-pub mod parse_cli_args;
+pub mod cat;
 pub mod cat_options;
 pub mod file_content;
+pub mod parse_cli_args;
