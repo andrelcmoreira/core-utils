0bf7640172f9d24dc7b62a7909dd884fd4c227d3
André L. C. Moreira <andrelcmoreira@disroot.org>
2024-02-15 00:28:02 -0300

    changing project's folder layout

---
+++ src/cat/src/tests/cat.rs
@@ -0,0 +1,44 @@
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn read_file_with_no_opts() {
+        // TODO
+    }
+
+    #[test]
+    fn read_file_with_squeeze_blank() {
+        // TODO
+    }
+
+    #[test]
+    fn read_file_with_show_ends() {
+        // TODO
+    }
+
+    #[test]
+    fn read_file_with_add_tabs() {
+        // TODO
+    }
+
+    #[test]
+    fn read_file_with_add_cr() {
+        // TODO
+    }
+
+    #[test]
+    fn read_file_with_number_non_blank() {
+        // TODO
+    }
+
+    #[test]
+    fn read_file_with_show_line_number() {
+        // TODO
+    }
+
+    #[test]
+    fn read_file_with_show_line_number_and_number_non_blank() {
+        // TODO
+    }
+}
